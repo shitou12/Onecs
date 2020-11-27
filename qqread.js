31ab3fc9d310d9399e716da0ace9fd3bc9179844
shitou12 <68673837+shitou12@users.noreply.github.com>
2020-11-27 12:05:08 +0000

    Update qqread.js

--- qqread.js
+++ qqread.js
@@ -52,6 +52,38 @@ const cookiesArr = [
       "Accept-Language": "zh-cn",
       "Accept-Encoding": "gzip, deflate, br",
       mpversion: "0.29.4",
+      }),
+  },
+  {
+    qqreadheaderVal: JSON.stringify({
+      ywsession: "1w6luxrauwft4xcpfkhxbcdxsmueay8r",
+      Cookie:
+        "ywguid=1499399062;ywkey=ywMJ8n8iEr3y;platform=ios;channel=mqqmina;mpVersion=0.29.4",
+      Connection: "keep-alive",
+      "Content-Type": "application/json",
+      Accept: "*/*",
+      Host: "mqqapi.reader.qq.com",
+      "User-Agent": "QQ/8.4.17.638 CFNetwork/1121.2.2 Darwin/19.2.0",
+      Referer: "https://appservice.qq.com/1110657249/0.29.4/page-frame.html",
+      "Accept-Language": "zh-cn",
+      "Accept-Encoding": "gzip, deflate, br",
+      mpversion: "0.29.4",
+    }),
+    qqreadtimeurlVal:
+      "https://mqqapi.reader.qq.com/mqq/addReadTimeWithBid?scene=1007&refer=-1&bid=26134185&readTime=9485&read_type=0&conttype=1&read_status=0&chapter_info=%5B%7B%221%22%3A%7B%22readTime%22%3A6063%2C%22pay_status%22%3A0%2C%22is_tail%22%3A0%7D%7D%2C%7B%222%22%3A%7B%22readTime%22%3A3422%2C%22pay_status%22%3A0%7D%7D%5D&sp=-1",
+    qqreadtimeheaderVal: JSON.stringify({
+      ywsession: "hf2t7p6inx8bfzd970v42l2m3yxgm5n8",
+      Cookie:
+        "ywguid=1499399062;ywkey=ywMJ8n8iEr3y;platform=ios;channel=mqqmina;mpVersion=0.29.4;qq_ver=8.4.17;os_ver=iOS 13.3;mpos_ver=1.21.0;platform=ios;openid=C1C065D4B40C71B3096EE3E481A2C912",
+      Connection: "keep-alive",
+      "Content-Type": "application/json",
+      Accept: "*/*",
+      Host: "mqqapi.reader.qq.com",
+      "User-Agent": "QQ/8.4.17.638 CFNetwork/1121.2.2 Darwin/19.2.0",
+      Referer: "https://appservice.qq.com/1110657249/0.29.4/page-frame.html",
+      "Accept-Language": "zh-cn",
+      "Accept-Encoding": "gzip, deflate, br",
+      mpversion: "0.29.4",
     }),   
   },
   
