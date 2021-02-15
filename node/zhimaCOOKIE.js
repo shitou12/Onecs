9020597f2e862d3a60383398f7eb602a9b634172
shitou12 <68673837+shitou12@users.noreply.github.com>
2021-02-15 22:36:23 +0800

    Create zhimaCOOKIE.js

---
+++ node/zhimaCOOKIE.js
@@ -0,0 +1,15 @@
+
+
+
+//独立COOKIE文件     ck在``里面填写，多账号换行
+
+let zhimabodyVal= `appversion=10&desarr=B9UriK6MqhbGjEJFP3eNKnGZOgPLMKoE0LqUPC7rhuvRBwh5rmsJ85GcyX87%2BNssL1EvonhUmpN07J1H6OW9MpP5Vvvl4Yq5CQakhWuIILqxcpLIreGshvXGPjmzxk7Aj9a%2BpVqYmNLAP%2BgNIsdQDaM5Qm3nKh1BjXzUlNawl%2B9mgyR9hPZOjVWkagrAJfdwDlyacfk0ZOgPXuXmK9edHIRS6Wg/S8A3d7%2Bmg6gWpzKzXgTsYKM/cyyCb6v09fXo9Bo1gbwmIfw5ofg/N%2B1fZxcHbAHGkR48LMVo/1AKLsdNGM4i3ysobsJAGHmlqSpHpRUZ10MJCuF3wJTUJi68evTno/QVQJWRA6U4CqyFqxDiPV/UvNjQgRoXaa1j7BFHdgrNWVaEYnsWc8zTRRia9yvhSubHzCtf5cvCwNKRrTIRz4TNl%2B8G5p4m6vxD7QV5TN8sQpjsOJFG5ydIOM/QvHss5KoBVkPfGKP8m3APHQ22Fad28/CfQO2FEyghELNkx9IR47AMBFo%2BnRIbL5T54x9hty6Ba8KRx3J`
+
+
+let zhimacookie = {
+
+  zhimabodyVal: zhimabodyVal,  
+  
+}
+
+module.exports =  zhimacookie
