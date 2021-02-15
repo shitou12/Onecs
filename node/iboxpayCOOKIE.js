7d63d0ae20daa73fe3d1c4d20daeaab868efa87c
shitou12 <68673837+shitou12@users.noreply.github.com>
2021-02-15 17:00:53 +0800

    Create iboxpayCOOKIE.js

---
+++ node/iboxpayCOOKIE.js
@@ -0,0 +1,18 @@
+
+
+//独立COOKIE文件     ck在``里面填写，多账号换行
+
+let refreshtokenVal= `358d40b92b2248d19333b095aa01f026
+c3136735892947798a96a025592ed42a
+52d2ec58999145a98f301936ec675edf
+83b6c198a9014f86886cd2edcc8348c3`
+
+
+let iboxpaycookie = {
+
+  refreshtokenVal: refreshtokenVal,  
+  
+}
+
+module.exports =  iboxpaycookie
+  
